5689b2e9f26649cd2a509c7abe7812dd3edf7b55
Lauri Hintsala <lauri.hintsala@verkkopaja.fi>
2021-04-21 23:03:40 +0300

    WIP: update sample app

--- examples/django/app/schema.py
+++ examples/django/app/schema.py
@@ -1,9 +1,30 @@
 import strawberry
+import strawberry_django
+from strawberry_django import mutations
 from typing import List
-from .types import Fruit
+from .types import (
+    Fruit, FruitInput, FruitPartialInput,
+    Color, ColorInput, ColorPartialInput,
+)
 
 @strawberry.type
 class Query:
-    fruits: List[Fruit]
+    fruit: Fruit = strawberry_django.field()
+    fruits: List[Fruit] = strawberry_django.field()
 
-schema = strawberry.Schema(query=Query)
+    color: Color = strawberry_django.field()
+    colors: List[Color] = strawberry_django.field()
+
+@strawberry.type
+class Mutation:
+    createFruit: Fruit = mutations.create(FruitInput)
+    createFruits: List[Fruit] = mutations.create(FruitInput, many=True)
+    updateFruits: List[Fruit] = mutations.update(FruitPartialInput, many=True)
+    deleteFruits: List[Fruit] = mutations.delete(many=True)
+
+    createColor: Color = mutations.create(ColorInput)
+    createColors: List[Color] = mutations.create(ColorInput, many=True)
+    updateColors: List[Color] = mutations.update(ColorPartialInput, many=True)
+    deleteColors: List[Color] = mutations.delete(many=True)
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
--- examples/django/app/types.py
+++ examples/django/app/types.py
@@ -14,3 +14,23 @@ class Color:
     id: auto
     name: auto
     fruits: List[Fruit]
+
+@strawberry_django.input(models.Fruit)
+class FruitInput:
+    id: auto
+    name: auto
+    color: auto
+
+@strawberry_django.input(models.Fruit, partial=True)
+class FruitPartialInput(FruitInput):
+    pass
+
+@strawberry_django.input(models.Color)
+class ColorInput:
+    id: auto
+    name: auto
+    fruits: auto
+
+@strawberry_django.input(models.Color, partial=True)
+class ColorPartialInput(ColorInput):
+    pass
